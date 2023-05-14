818656fa2677f6d96763a45b05574ca0a18ccfb6
arifulgony <arifulgony@gmail.com>
2023-05-14 20:49:32 +0600

    explore

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = () => {
     return (
         <div>
-           <div className="navbar bg-base-100 ">
+           <div className="navbar bg-base-100">
               <div className="navbar-start">
                 <div className="dropdown">
                   <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -63,6 +63,7 @@ const NavBar = () => {
                 </label>
                </div>
             </div>
+            <hr className='bg-[#3F3F3F]' />
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../Header/NavBar';
+import Explore from '../Explore/Explore';
 
 const Home = () => {
     return (
         <div>
             <NavBar/>
+            <Explore/>
         </div>
     );
 };
