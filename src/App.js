ea9c71dcc270f2d88cc3125b78ca52a93704fa89
arifulgony <arifulgony@gmail.com>
2023-05-14 21:17:46 +0600

    banner

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="w-[1440px] mx-auto">
+    <div className="w-[1440px] mx-auto ">
       <Home/>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavBar from '../Header/NavBar';
 import Explore from '../Explore/Explore';
+import Banner from '../Banner/Banner';
+
 
 const Home = () => {
     return (
         <div>
             <NavBar/>
+            <Banner/>
             <Explore/>
         </div>
     );
