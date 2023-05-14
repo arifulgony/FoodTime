6ee13aa0748a3278a9dd2c1390815190b6b8638d
arifulgony <arifulgony@gmail.com>
2023-05-14 18:36:41 +0600

    navbar

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../Header/NavBar';
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    );
+};
+
+export default Home;
